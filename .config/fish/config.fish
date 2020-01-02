0d73d9590262dedb21d0fdcc5767b2db78291fa6
Adam Adamsky <adam@adms.ky>
2020-01-02 14:01:34 +0100

    alias vim to nvim, start tmux at startup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,5 @@
 alias cfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias vim nvim
+
+test $TERM != "screen"; and exec tmux
